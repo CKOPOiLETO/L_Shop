c6a98f4f55c73d654c92d48aa4f829bb86b618db
hashtaggedtodeath <145613017+hashtaggedtodeath@users.noreply.github.com>
2026-03-05 17:50:11 +0300

    фронт

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { Header } from './components/Header';
+import { MainPage } from './pages/MainPage';
+import { LoginPage } from './pages/LoginPage';
+import { CartPage } from './pages/CartPage';
+import { DeliveryPage } from './pages/DeliveryPage'; // <--- Импорт
+import { AuthProvider } from './contexts/AuthContext';
+import { CartProvider } from './contexts/CartContext';
 
 function App() {
   return (
-    <Routes>
-      <Route>
-          
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <CartProvider>
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/delivery" element={<DeliveryPage />} /> {/* <--- Маршрут */}
+          </Routes>
+        </div>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
